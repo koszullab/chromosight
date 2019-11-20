7c8fbd5873384f7913a6e7dce480f6320e899978
cmdoret <cyril.mattheydoret@gmail.com>
2019-11-20 22:20:46 +0100

    write all tests for the plotting module

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,34 +1,49 @@
 from nose2.tools import params
+import tempfile
+import os
+import numpy as np
 import chromosight.utils.plotting as cup
 import chromosight.utils.detection as cud
+import chromosight.utils.io as cio
 
 
 mat, chroms, bins, res = cio.load_cool("data_test/example.cool")
+
 mat = mat.tocsr()
 #  Get all intra-chromosomal matrices
 intra_mats = [
     mat[s:e, s:e] for s, e in zip(chroms["start_bin"], chroms["end_bin"])
 ]
+
 pattern_list = []
 window_list = []
 
 
-@params(*zip(intra_mats, pattern_list))
-def test_pattern_plot(contact_map, patterns):
-    cup.pattern_plot(contact_map, patterns, output=None, name=None)
+class TestPlotting:
+    def __init__(self):
+        """Setup function to generate a named tempfile"""
+        # Create tmp temporary file for reading and writing
+        tmp_out = tempfile.NamedTemporaryFile(delete=False)
+        tmp_out.close()
+        # Give access to full path, dirname and basename in diff variables
+        self.tmp_path = tmp_out.name
+        self.tmp_dir = os.path.dirname(self.tmp_path)
+        self.tmp_file = os.path.basename(self.tmp_path)
+
+    def test_distance_plot(self):
+        cup.distance_plot(intra_mats, labels=None, out=self.tmp_path)
+
+    def test_pileup_plot(self):
+        windows = np.reshape(np.random.randint(100, size=1000), (10, 10, 10))
+        pileup_pattern = cud.pileup_patterns(windows)
+        cup.pileup_plot(
+            pileup_pattern, name="pileup_patterns", output=self.tmp_dir
+        )
 
 
-def test_distance_plot():
-    cup.distance_plot(intra_mats, labels=None)
-
-
-@params(*window_list)
-def test_pileup_plot(windows):
-    pileup_pattern = cud.pileup_patterns(windows)
-    cup.pileup_plot(pileup_pattern, name="pileup patterns", output=None)
-
-
+"""
 @params(*zip(pattern_list, chroms.name))
 def test_plot_whole_matrix(patterns, chrom):
     region = chroms.loc[chroms.name == chrom, ["start_bin", "end_bin"]]
     cup.plot_whole_matrix(mat, patterns, out=None, region=region)
+"""
