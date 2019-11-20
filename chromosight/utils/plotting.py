6880d1777cf9ce87c4ec0f62966c42b111f1f41c
cmdoret <cyril.mattheydoret@gmail.com>
2019-11-20 22:20:03 +0100

    add isotonic regression smoothing in distance law plots

--- chromosight/utils/plotting.py
+++ chromosight/utils/plotting.py
@@ -1,62 +1,12 @@
 import pathlib
 import functools
 import numpy as np
+import chromosight.utils.preprocessing as preproc
 from matplotlib import pyplot as plt
+from sklearn.isotonic import IsotonicRegression
 
 
-def pattern_plot(contact_map, patterns, output=None, name=None):
-    """Plot patterns
-
-    Plot the matrix with the positions of the found patterns (border or loop)
-    on it.
-
-    Parameters
-    ----------
-    contact_map : chromosight.utils.contact_map.ContactMap
-        Object containing all information related to a Hi-C contact map.
-    patterns : dict
-        Dictionary object storing all patterns to be plotted. Structured as :
-        {pattern_type: []}
-    """
-    if name is None:
-        name = 0
-    if output is None:
-        output = pathlib.Path()
-    else:
-        output = pathlib.Path(output)
-
-    plt.imshow(
-        contact_map.mat.todense() ** 0.15,
-        interpolation="none",
-        cmap="afmhot_r",
-    )
-    plt.title(name, fontsize=8)
-    plt.colorbar()
-
-    for pattern_type, all_patterns in patterns.items():
-        if pattern_type == "borders":
-            for border in all_patterns:
-                if border[0] != name:
-                    continue
-                if border[1] != "NA":
-                    _, pos1, pos2, _ = border
-                    plt.plot(pos1, pos2, "D", color="green", markersize=0.1)
-        elif pattern_type == "loops":
-            for loop in all_patterns:
-                if loop[0] != name:
-                    continue
-                if loop[1] != "NA":
-                    _, pos1, pos2, _ = loop
-                    plt.scatter(
-                        pos1, pos2, s=15, facecolors="none", edgecolors="blue"
-                    )
-
-    emplacement = output / pathlib.Path(str(name + 1) + ".2.pdf")
-    plt.savefig(emplacement, dpi=100, format="pdf")
-    plt.close("all")
-
-
-def distance_plot(matrices, labels=None):
+def distance_plot(matrices, labels=None, out=None, smooth=True):
 
     if isinstance(matrices, np.ndarray):
         matrix_list = [matrices]
@@ -69,25 +19,27 @@ def distance_plot(matrices, labels=None):
         labels = [labels]
 
     for matrix, name in zip(matrix_list, labels):
-        dist = utils.distance_law(matrix)
+        dist = preproc.distance_law(matrix, fun=np.nanmean)
         x = np.arange(0, len(dist))
         y = dist
+        if smooth:
+            ir = IsotonicRegression(increasing=False)
+            y = ir.fit_transform(x, y, sample_weight=1 / (1 + x))
         y[np.isnan(y)] = 0.0
-        y_savgol = savgol_filter(y, window_length=17, polyorder=5)
-        plt.plot(x, y, "o")
-        plt.plot(x)
-        plt.plot(x, y_savgol)
+        plt.plot(x, y, label=str(name))
         plt.xlabel("Genomic distance")
         plt.ylabel("Contact frequency")
-        plt.xlim(10 ** 0, 10 ** 3)
-        plt.ylim(10 ** -5, 10 ** -1)
+        # plt.xlim(10 ** 0, 10 ** 3)
+        # plt.ylim(10 ** -5, 10 ** -1)
         plt.loglog()
-        plt.title(name)
-        plt.savefig(pathlib.Path(name) / ".pdf3", dpi=100, format="pdf")
-        plt.close("all")
+    plt.legend()
+    if out is None:
+        plt.show()
+    else:
+        plt.savefig(out)
 
 
-def pileup_plot(pileup_pattern, name="pileup patterns", output=None):
+def pileup_plot(pileup_pattern, name="pileup_patterns", output=None):
     """
     Plot the pileup of all detected patterns
     """
@@ -103,7 +55,7 @@ def pileup_plot(pileup_pattern, name="pileup patterns", output=None):
         vmax=2.0,
         cmap="seismic",
     )
-    plt.title("pileup {}".format(name))
+    plt.title("{} pileup".format(name))
     plt.colorbar()
     emplacement = output / pathlib.Path(name + ".pdf")
     plt.savefig(emplacement, dpi=100, format="pdf")
