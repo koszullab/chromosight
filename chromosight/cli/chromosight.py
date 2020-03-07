a9db85987a4fd75a374d58fc70e357ca82db0933
cmdoret <cyril.mattheydoret@gmail.com>
2020-03-07 12:29:39 +0100

    only use a multiprocessing dispatcher when multiple threads are requested

--- chromosight/cli/chromosight.py
+++ chromosight/cli/chromosight.py
@@ -546,14 +546,16 @@ def cmd_detect(arguments):
             # Run detection in parallel on different sub matrices, and show progress when
             # gathering results
             sub_mat_results = []
-            for s, result in enumerate(
-                pool.imap_unordered(_detect_sub_mat, sub_mat_data, 1)
-            ):
-                chr1 = hic_genome.sub_mats.chr1[s]
-                chr2 = hic_genome.sub_mats.chr2[s]
-                cio.progress(s, n_sub_mats, f"{chr1}-{chr2}")
-                sub_mat_results.append(result)
-            # sub_mat_results = list(map(_detect_sub_mat, sub_mat_data))
+            # Run in multiprocessing subprocesses
+            if threads > 1:
+                dispatcher = pool.imap_unordered(_detect_sub_mat, sub_mat_data, 1)
+            else:
+                dispatcher = map(_detect_sub_mat, sub_mat_data)
+                for s, result in enumerate(dispatcher):
+                    chr1 = hic_genome.sub_mats.chr1[s]
+                    chr2 = hic_genome.sub_mats.chr2[s]
+                    cio.progress(s, n_sub_mats, f"{chr1}-{chr2}")
+                    sub_mat_results.append(result)
 
             # Convert coordinates from chromosome to whole genome bins
             kernel_coords = [
