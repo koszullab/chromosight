383f4317a8b14f31e2e955a0d57416bfd3c4951c
cmdoret <cyril.mattheydoret@gmail.com>
2020-06-24 14:03:37 +0200

    add favicon to readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = "alabaster"
-
+html_favicon = 'logo/chromosight_favicon.png'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
