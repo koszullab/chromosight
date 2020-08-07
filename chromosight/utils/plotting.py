b0cdaa59c578e1882b9708afcb914dfd23c2cb6d
cmdoret <cyril.mattheydoret@gmail.com>
2020-08-07 12:30:50 +0200

    rewrite utility function for plotting matrix + patterns so that it uses range selectors

--- chromosight/utils/plotting.py
+++ chromosight/utils/plotting.py
@@ -27,7 +27,7 @@ def pileup_plot(pileup_pattern, output_prefix, name="pileup_patterns"):
 
 
 def plot_whole_matrix(
-    mat, patterns, out=None, region=None, region2=None, log_transform=False
+    clr, patterns, out=None, region=None, region2=None, log_transform=False
 ):
     """
     Visualise the input matrix with a set of patterns overlaid on top.
@@ -41,41 +41,46 @@ def plot_whole_matrix(
         The set of patterns to be plotted on top of the matrix. One pattern per
         row, 3 columns: bin1, bin2 and score of types int, int and float,
         respectively.
-    region : tuple of ints
-        The range of rows to be plotted in the matrix. If not given, the whole
-        matrix is used. It only region is given, but not region2, the matrix is
-        subsetted on rows and columns to show a region on the diagonal.
-    region2 : tuple of ints
-        The range of columns to be plotted in the matrix. Region must also be
-        provided, or this will be ignored.
+    region : str
+        The genomic range, in UCSC format, corresponding to rows to be plotted
+        in the matrix. If not given, the whole matrix is used. It only region
+        is given, but not region2, the matrix is subsetted on rows and columns
+        to show a region on the diagonal.
+    region2 : str
+        The genomic range, in UCSC format, of columns to be plotted in the matrix.
+        Region must also be provided, or this will be ignored.
     log_transform : bool
         Whether to log transform the matrix.
 
     """
+
     if region is not None and region2 is None:
-        s1, e1 = region
+        mat = clr.matrix().fetch(region)
+        s1, e1 = clr.extent(region)
         s2, e2 = s1, e1
     elif region is not None and region2 is not None:
-        s1, e1 = region
-        s2, e2 = region2
+        mat = clr.matrix().fetch(region, region2)
+        s1, e1 = clr.extent(region)
+        s2, e2 = clr.extent(region2)
     else:
-        s1, e1 = 0, mat.shape[0]
-        s2, e2 = 0, mat.shape[1]
+        mat = clr.matrix()[:]
+        s1, e1 = 0, clr.shape[0]
+        s2, e2 = 0, clr.shape[1]
 
     pat = patterns.copy()
     pat = pat.loc[
         (pat.bin1 > s1) & (pat.bin1 < e1) & (pat.bin2 > s2) & (pat.bin2 < e2),
         :,
     ]
-    sub_mat = mat.tocsr()[s1:e1, s2:e2].todense()
+    
     if log_transform:
-        sub_mat = np.log(sub_mat)
-    sub_mat[sub_mat == 0] = np.nan
+        mat = np.log(mat)
+    mat[mat == 0] = np.nan
     plt.figure(dpi=1200)
     plt.imshow(
-        sub_mat,
+        mat,
         cmap="Reds",
-        vmax=np.percentile(sub_mat[~np.isnan(sub_mat)], 99),
+        vmax=np.percentile(mat[~np.isnan(mat)], 99),
     )
     plt.scatter(
         pat.bin1 - s1,
